999cd73dbd20407f4a7177ed97ad78ef52c0e440
richard-ukpabi <richardukpabi25@gmail.com>
2026-03-02 09:03:46 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
-// Example: index.js
+// index.js
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {res.json({ message: ' App is running shit ' });
+// Optional: enable JSON parsing for POST requests
+// app.use(express.json());
 
- });
+app.get('/', (req, res) => {
+  res.json({ message: 'App is running' });
+});
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
-
 });
